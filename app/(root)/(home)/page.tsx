290831186d52c28dbae38af1b27f430644d5e0c3
Milena Pieńkosz <designbymilencja@gmail.com>
2023-12-19 18:19:40 +0300

    created develop

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,7 +1,30 @@
- 
-export default function Home() {
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Home | Dev Overflow",
+  description:
+    "Dev Overflow is a community for developers to share their knowledge and experience.",
+};
+export default async function Home() {
+  
+
   return (
-    <div>
-    </div>
-  )
+    <>
+      <div className="flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center">
+        <h1 className="h1-bold text-dark100_light900">All Questions</h1>
+        <Link href={"/ask-question"} className="flex justify-end max-sm:w-full">
+          <Button
+            className="primary-gradient min-h-[46px] px-4 py-3
+           !text-light-900"
+          >
+            Ask a Question
+          </Button>
+        </Link>
+      </div>
+     
+    </>
+  );
 }
