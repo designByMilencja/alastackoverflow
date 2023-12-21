2e8502f3074171b817159190b1dbfad680471a3c
Milena Pieńkosz <designbymilencja@gmail.com>
2023-12-21 20:14:59 +0300

    fixed bugs

--- components/home/HomeFilters.tsx
+++ components/home/HomeFilters.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { HomePageFilters } from "@/constants/filters";
 import { Button } from "@/components/ui/button";
-import { useState } from "react";
+// import { useState } from "react";
 // import { useRouter, useSearchParams } from "next/navigation";
 // import { formUrlQuery } from "@/lib/utils";
 
