526cc78001cd417788cf02e14061de6d786b978a
Milena Pieńkosz <designbymilencja@gmail.com>
2023-12-21 13:22:03 +0300

    created filter and local search component

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,15 +1,52 @@
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
-
+// import LocalSearchbar from "@/components/shared/search/LocalSearchbar";
+// import Filter from "@/components/shared/Filter";
+// import { HomePageFilters } from "@/constants/filters";
+// import HomeFilters from "@/components/home/HomeFilters";
+// import NoResult from "@/components/shared/NoResult";
+// import QuestionCard from "@/components/cards/QuestionCard";
+// import {
+//   getQuestions,
+//   getRecommendedQuestions,
+// } from "@/lib/actions/question.action";
+import { SearchParamsProps } from "@/types";
+// import Pagination from "@/components/shared/Pagination";
 import type { Metadata } from "next";
+import LocalSearch from "@/components/shared/search/LocalSearch";
+import Filter from "@/components/shared/Filter";
+import { HomePageFilters } from "@/constants/filters";
+// import { auth } from "@clerk/nextjs";
 
 export const metadata: Metadata = {
   title: "Home | Dev Overflow",
   description:
     "Dev Overflow is a community for developers to share their knowledge and experience.",
 };
-export default async function Home() {
-  
+export default async function Home({ searchParams }: SearchParamsProps) {
+  // const { userId } = auth();
+  // let result;
+
+  // if (searchParams?.filter === "recommended") {
+  //   if (userId) {
+  //     result = await getRecommendedQuestions({
+  //       searchQuery: searchParams.q,
+  //       page: searchParams.page ? +searchParams.page : 1,
+  //       userId,
+  //     });
+  //   } else {
+  //     result = {
+  //       questions: [],
+  //       isNext: false,
+  //     };
+  //   }
+  // } else {
+  //   result = await getQuestions({
+  //     searchQuery: searchParams.q,
+  //     filter: searchParams.filter,
+  //     page: searchParams.page ? +searchParams.page : 1,
+  //   });
+  // }
 
   return (
     <>
@@ -24,7 +61,53 @@ export default async function Home() {
           </Button>
         </Link>
       </div>
-     
+      <div className="mt-11 flex justify-between gap-5 max-sm:flex-col sm:items-center">
+        <LocalSearch
+          route="/"
+          iconPosition="left"
+          imgSrc="/assets/icons/search.svg"
+          placeholder="Search for questions"
+          otherClasses="flex-1"
+        />
+        <Filter
+          filters={HomePageFilters}
+          otherClasses="min-h-[56px] sm:min-w-[170px]"
+          containerClasses="hidden max-md:flex"
+        />
+      </div>
+      {/* <HomeFilters /> */}
+      {/* <div className="mt-10 flex w-full flex-col gap-6">
+        {result.questions.length > 0 ? (
+          result.questions.map((question) => (
+            <QuestionCard
+              key={question._id}
+              _id={question._id}
+              title={question.title}
+              tags={question.tags}
+              author={question.author}
+              upvotes={question.upvotes}
+              views={question.views}
+              answers={question.answers}
+              createdAt={question.createdAt}
+            />
+          ))
+        ) : (
+          <NoResult
+            title="There's no question to show"
+            description="Be the first to break the silence! 🚀 Ask a Question and kickstart the
+        discussion. our query could be the next big thing others learn from. Get
+        involved! 💡"
+            link={"/ask-questions"}
+            linkTitle="Ask a Question"
+          />
+        )}
+      </div> */}
+      {/* <div className="mt-10">
+        <Pagination
+          pageNumber={searchParams?.page ? +searchParams.page : 1}
+          isNext={result.isNext}
+        />
+      </div> */}
     </>
   );
 }
